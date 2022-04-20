bb7bb9b557a5db07422c90640acf976def1d2e41
afagan5 <abby.fagan@kellogg.northwestern.edu>
2022-04-20 05:06:16 +0000

    updates tuesday night

--- kmdb.sql
+++ kmdb.sql
@@ -152,14 +152,14 @@ studio_name TEXT
 );
 
 
-INSERT INTO movies (title, year_released, MPAA_rating)
-VALUES ("Batman Begins", "2005", "PG-13");
+INSERT INTO movies (title, year_released, MPAA_rating, studio_id, actor_id)
+VALUES ("Batman Begins", "2005", "PG-13", "1", "1");
 
-INSERT INTO movies (title, year_released, MPAA_rating)
-VALUES ("The Dark Knight", "2008", "PG-13");
+INSERT INTO movies (title, year_released, MPAA_rating, studio_id, actor_id)
+VALUES ("The Dark Knight", "2008", "PG-13", "1", "1");
 
-INSERT INTO movies (title, year_released, MPAA_rating)
-VALUES ("The Dark Knight Rises", "2012", "PG-13");
+INSERT INTO movies (title, year_released, MPAA_rating, studio_id, actor_id)
+VALUES ("The Dark Knight Rises", "2012", "PG-13", "1", "1");
 
 INSERT INTO actors (actor_name, character_name)
 VALUES ("Christian Bale", "Bruce Wayne");
@@ -194,4 +194,8 @@ VALUES ("Joseph Gordon-Levitt", "John Blake");
 INSERT INTO actors (actor_name, character_name)
 VALUES ("Anne Hathaway", "Selina Kyle");
 
+INSERT INTO studios (studio_name)
+VALUES ("Warner Bros.");
 
+SELECT movies.title, movies.year_released, movies.MPAA_rating, studios.studio_name
+FROM movies INNER JOIN studios ON studios.id = movies.studio_id;
